d81f35732387a3bf90d657120a29ff6063889b8e
hunterrudnet <hunter.rudnet@gmail.com>
2023-02-11 12:05:41 -0500

    fix navigation sidebar

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -6,49 +6,59 @@ const NavigationSidebar = (
     }
 ) => {
     return (
+        <>
         <div className="list-group">
-            <a className="list-group-item">Tuiter</a>
-            <a className={`list-group-item
+            <a className="list-group-item list-group-item-action">
+                <i className="bi bi-twitter"></i>
+            </a>
+            <a className={`list-group-item list-group-item-action
                     ${active === 'home'?'active':''}`}>
                 <i className="bi bi-house-door-fill"></i>
-                Home
+                <span className="d-none d-xl-inline">Home</span>
             </a>
-            <a className={`list-group-item
+            <a className={`list-group-item list-group-item-action
                     ${active === 'explore'?'active':''}`}>
                 <i className="bi bi-hash"></i>
-                Explore
+                <span className="d-none d-xl-inline">Explore</span>
             </a>
-            <a className={`list-group-item
+            <a className={`list-group-item list-group-item-action
                     ${active === 'notifications'?'active':''}`}>
                 <i className="bi bi-bell-fill"></i>
-                Notifications
+                <span className="d-none d-xl-inline">Notifications</span>
             </a>
-            <a className={`list-group-item
+            <a className={`list-group-item list-group-item-action
                     ${active === 'messages'?'active':''}`}>
                 <i className="bi bi-envelope-fill"></i>
-                Messages
+                <span className="d-none d-xl-inline">Messages</span>
             </a>
-            <a className={`list-group-item
+            <a className={`list-group-item list-group-item-action
                     ${active === 'bookmarks'?'active':''}`}>
                 <i className="bi bi-bookmark-fill"></i>
-                Bookmarks
+                <span className="d-none d-xl-inline">Bookmarks</span>
             </a>
-            <a className={`list-group-item
+            <a className={`list-group-item list-group-item-action
                     ${active === 'lists'?'active':''}`}>
                 <i className="bi bi-list"></i>
-                Lists
+                <span className="d-none d-xl-inline">Lists</span>
             </a>
-            <a className={`list-group-item
+            <a className={`list-group-item list-group-item-action
                     ${active === 'profile'?'active':''}`}>
                 <i className="bi bi-person-fill"></i>
-                Profile
+                <span className="d-none d-xl-inline">Profile</span>
             </a>
-            <a className={`list-group-item
+            <a className={`list-group-item list-group-item-action
                     ${active === 'more'?'active':''}`}>
                 <i className="bi bi-three-dots"></i>
-                More
+                <span className="d-none d-xl-inline">More</span>
             </a>
         </div>
+        <div className="d-grid mt-2">
+            <a href="tweet.html"
+               className="btn btn-primary btn-block rounded-pill">
+                Tweet</a>
+        </div>
+        </>
+
     );
 };
 export default NavigationSidebar;
