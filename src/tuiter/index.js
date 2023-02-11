9d5f8815a0b3dd2553275f6a7e29b6a7d27e025c
hunterrudnet <hunter.rudnet@gmail.com>
2023-02-11 12:15:04 -0500

    touches

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -9,11 +9,11 @@ function Tuiter() {
             <div className="col-2 col-md-2 col-lg-1 col-xl-2">
                 <NavigationSidebar active="explore"/>
             </div>
-            <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+            <div className="col-10 col-lg-7 col-xl-6"
                  style={{"position": "relative"}}>
                 <ExploreComponent/>
             </div>
-            <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+            <div className="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 <WhoToFollowList/>
             </div>
         </div>
