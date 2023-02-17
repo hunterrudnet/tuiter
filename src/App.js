5a027a665441de4367698c13c4537127712beee7
hunterrudnet <hunter.rudnet@gmail.com>
2023-02-17 10:46:29 -0500

    start a7

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <BrowserRouter>
             <div className="container">
                 <Routes>
-                    <Route index
+                    <Route path="/*"
                            element={<Labs/>}/>
                     <Route path="/hello"
                            element={<HelloWorld/>}/>
--- src/nav.js
+++ src/nav.js
@@ -3,11 +3,12 @@ import {Link} from "react-router-dom";
 function Nav() {
     return (
         <div>
-            <Link to="/">Lab</Link> |
-            <Link to="/hello">Hello</Link> |
+            <Link to="/">Labs</Link> |
+            <Link to="/">Assignment 6</Link> |
+            <Link to="/a7">Assignment 7</Link> |
             <Link to="/tuiter">Tuiter</Link>
         </div>
-    )
+    );
 }
 
 export default Nav;
