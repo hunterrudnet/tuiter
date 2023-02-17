292dca7db838cad7222d2981944d0bcb7906c9aa
hunterrudnet <hunter.rudnet@gmail.com>
2023-02-17 14:38:34 -0500

    rough draft

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -1,5 +1,17 @@
-import React from "react";
+import {React, useState} from "react";
 const TuitStats = ({tuit}) => {
+    const [liked, setLiked] = useState(tuit.liked);
+    const [likes, setLikes] = useState(tuit.likes);
+
+    const likeHandler = () => {
+        if (liked) {
+            setLikes(likes - 1);
+        } else {
+            setLikes(likes + 1);
+        }
+        setLiked(!liked);
+    }
+
     return(
         <div className="nav nav-justified mt-2 p-0">
             <div className="nav-item">
@@ -10,13 +22,13 @@ const TuitStats = ({tuit}) => {
                 <i className="bi bi-arrow-repeat m-2"></i>
                 <span className="text-muted">{tuit.retuits}</span>
             </div>
-            <div className="nav-item">
-                {tuit.liked ?
+            <div className="nav-item" onClick={likeHandler}>
+                {liked ?
                     <i className="bi bi-heart-fill m-2"></i>
                     :
                     <i className="bi bi-heart m-2"></i>
                 }
-                <span className="text-muted">{tuit.likes}</span>
+                <span className="text-muted">{likes}</span>
             </div>
             <div className="nav-item">
                 <i className="bi bi-upload m-2"></i>
