c379d72af585e7f2a23022daaee8c3d3c0b15ea3
hunterrudnet <hunter.rudnet@gmail.com>
2023-02-17 14:03:20 -0500

    fix routing

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,18 +1,19 @@
 import React from "react";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
+import HomeComponent from "./home";
+import ExploreComponent from "./explore";
 import whoReducer from "./reducers/who-reducer";
 import tuitsReducer from "./tuits/tuits-reducer";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import HomeComponent from "./home";
+import {Route, Routes} from "react-router";
 const store = configureStore({
     reducer: {
         who: whoReducer,
         tuits: tuitsReducer
     }
 });
-
 function Tuiter() {
     return (
         <Provider store={store}>
@@ -20,9 +21,12 @@ function Tuiter() {
                 <div className="col-2 col-md-2 col-lg-1 col-xl-2">
                     <NavigationSidebar active="explore"/>
                 </div>
-                <div className="col-10 col-lg-7 col-xl-6"
-                     style={{"position": "relative"}}>
-                    <HomeComponent/>
+                <div className="col-10 col-lg-7 col-xl-6">
+                    <Routes>
+                        <Route path="/" element={<HomeComponent/>}/>
+                        <Route path="/home" element={<HomeComponent/>}/>
+                        <Route path="/explore" element={<ExploreComponent/>}/>
+                    </Routes>
                 </div>
                 <div className="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                     <WhoToFollowList/>
