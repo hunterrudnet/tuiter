d9dab3fa8bcc06af4f5563c63a2868f46cefbecf
hunterrudnet <hunter.rudnet@gmail.com>
2023-02-17 11:26:09 -0500

    finish 3.1

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -2,21 +2,32 @@ import React from "react";
 import ExploreComponent from "./explore";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
+import {Routes, Route} from "react-router";
+import whoReducer from "./reducers/who-reducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+const store = configureStore({
+    reducer: {
+        who: whoReducer
+    }
+});
 
 function Tuiter() {
     return (
-        <div className="row mt-2">
-            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                <NavigationSidebar active="explore"/>
+        <Provider store={store}>
+            <div className="row mt-2">
+                <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                    <NavigationSidebar active="explore"/>
+                </div>
+                <div className="col-10 col-lg-7 col-xl-6"
+                     style={{"position": "relative"}}>
+                    <ExploreComponent/>
+                </div>
+                <div className="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                    <WhoToFollowList/>
+                </div>
             </div>
-            <div className="col-10 col-lg-7 col-xl-6"
-                 style={{"position": "relative"}}>
-                <ExploreComponent/>
-            </div>
-            <div className="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-                <WhoToFollowList/>
-            </div>
-        </div>
+        </Provider>
     );
 }
 
