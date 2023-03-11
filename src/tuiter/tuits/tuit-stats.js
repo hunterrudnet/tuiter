dbd726e2d982276c24a463cca9dcc256e2e2f8e1
hunterrudnet <hunter.rudnet@gmail.com>
2023-03-11 13:28:17 -0500

    add dislike button and remove the local state

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -1,15 +1,40 @@
 import {React, useState} from "react";
+import { useDispatch } from "react-redux";
+import { updateTuitThunk } from "../../services/tuits-thunks";
+
 const TuitStats = ({tuit}) => {
     const [liked, setLiked] = useState(tuit.liked);
-    const [likes, setLikes] = useState(tuit.likes);
+    const [disliked, setDisiked] = useState(tuit.disliked);
+    const dispatch = useDispatch();
 
     const likeHandler = () => {
         if (liked) {
-            setLikes(likes - 1);
+            dispatch(updateTuitThunk({
+                ...tuit,
+                likes: tuit.likes - 1
+            }));
         } else {
-            setLikes(likes + 1);
+            dispatch(updateTuitThunk({
+                ...tuit,
+                likes: tuit.likes + 1
+            }));
         }
         setLiked(!liked);
+    }
+
+    const dislikeHandler = () => {
+        if (disliked) {
+            dispatch(updateTuitThunk({
+                ...tuit,
+                dislikes: tuit.dislikes - 1
+            }));
+        } else {
+            dispatch(updateTuitThunk({
+                ...tuit,
+                dislikes: tuit.dislikes + 1
+            }));
+        }
+        setDisiked(!disliked);
     }
 
     return(
@@ -22,13 +47,22 @@ const TuitStats = ({tuit}) => {
                 <i className="bi bi-arrow-repeat m-2"></i>
                 <span className="text-muted p-1">{tuit.retuits}</span>
             </div>
+
             <div className="nav-item w-25" onClick={likeHandler}>
                 {liked ?
                     <i className="bi bi-heart-fill text-danger m-2"></i>
                     :
                     <i className="bi bi-heart m-2"></i>
                 }
-                <span className="text-muted p-1">{likes}</span>
+                <span className="text-muted p-1">{tuit.likes}</span>
+            </div>
+            <div className="nav-item w-25" onClick={dislikeHandler}>
+                {disliked ?
+                    <i className="bi bi-hand-thumbs-down-fill text-primary m-2"></i>
+                    :
+                    <i className="bi bi-hand-thumbs-down m-2"></i>
+                }
+                <span className="text-muted p-1">{tuit.dislikes}</span>
             </div>
             <div className="nav-item w-25">
                 <i className="bi bi-upload m-2"></i>
--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -1,6 +1,6 @@
 import {createSlice}
   from "@reduxjs/toolkit";
-import {createTuitThunk, deleteTuitThunk, findTuitsThunk}
+import {createTuitThunk, deleteTuitThunk, updateTuitThunk, findTuitsThunk}
   from "../../services/tuits-thunks";
 
 const initialState = {
@@ -37,8 +37,17 @@ const tuitsSlice = createSlice({
         state.loading = false
         state.tuits.push(payload)
     },
+    [updateTuitThunk.fulfilled]:
+        (state, { payload }) => {
+            state.loading = false
+            const tuitNdx = state.tuits
+            .findIndex((t) => t._id === payload._id)
+            state.tuits[tuitNdx] = {
+            ...state.tuits[tuitNdx],
+            ...payload
+            }
+        }
     },
-
     reducers: {}
 });
 
