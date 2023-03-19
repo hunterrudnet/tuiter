db3590e405d437afbf2fbdd617de53e2240e9073
hunterrudnet <hunter.rudnet@gmail.com>
2023-03-18 22:56:05 -0400

    Add support for process.env.REACT_APP_API_BASE

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const TUITS_API = 'http://localhost:4000/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
