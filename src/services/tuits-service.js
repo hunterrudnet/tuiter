783bc5b80d0aac43d78739774552ec5a83cd2adc
hunterrudnet <hunter.rudnet@gmail.com>
2023-03-19 00:24:30 -0400

    nope

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE_A9; // putting back to work on netlify
+const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
